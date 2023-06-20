9413d8a7b16d91235849e953587498edd8ffd62c
Angelo <angelo99franciamore@gmail.com>
2023-06-20 17:02:26 +0200

    feat: added the dataset_2
    the dataset_2 need to calculate which municipalities have an high value of ag

--- input.py
+++ input.py
@@ -4,6 +4,15 @@ import pandas as pd
 import_dataset = pd.read_csv("classificazione_sismica.csv")
 
 select_columns = ['ag', 'Sigla Provincia', 'Nuova Classificazione sismica']
+select_columns2 = ['ag', 'Sigla Provincia','Comune', 'Nuova Classificazione sismica']
+
 dataset = import_dataset[select_columns]
+dataset_2 = import_dataset[select_columns2]
+
 dataset = dataset.rename(columns={'Sigla Provincia': 'province', 'Nuova Classificazione sismica': 'new_classification'})
-# print(dataset.columns)
+dataset_2 = dataset_2.rename(columns={'Sigla Provincia': 'province','Comune': 'municipality','Nuova Classificazione sismica': 'new_classification'})
+#Casting the ag columns from string to float
+dataset_2['ag'] = dataset_2['ag'].str.replace(",", ".")
+dataset_2['ag'] = dataset_2['ag'].astype('float')
+
+# print(dataset_2)
