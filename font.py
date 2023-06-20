42d99861be0f44d31963e20d23b83b26612a501a
Angelo <angelo99franciamore@gmail.com>
2023-06-20 13:11:11 +0200

    add: added font py for labels, title and ticks of the plots

---
+++ font.py
@@ -0,0 +1,19 @@
+# This font is for x and y labels
+font_labels = {'family': 'serif',
+               'color': 'darkred',
+               'weight': 'normal',
+               'size': 20,
+               }
+
+# This font is for the title of the plot
+font_title = {'family': 'sans-serif',
+              'color': 'Blue',
+              'weight': 'bold',
+              'size': 18,
+              }
+
+# This font is for the ticks of the plot
+font_ticks = {'family': 'sans-serif',
+              'color': 'Black',
+              'size': 14,
+              }
