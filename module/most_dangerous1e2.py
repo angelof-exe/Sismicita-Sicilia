3ae1f50cce70ca18f6f6626aca2e925faa00ea7c
Angelo <angelo99franciamore@gmail.com>
2023-06-23 16:32:06 +0200

    add: added new module
    the most_dangerous1e2.py file need to sort the municipalities that are in the level 1 and 2 of sismicity

---
+++ module/most_dangerous1e2.py
@@ -0,0 +1,15 @@
+# This file filters all municipalities with classification level 1 & 2
+# and calculates how many of these municipalities are present in the province
+from input import dataset
+
+# Group by Province and then count how many municipalities are classificated Level 1 and 2, then sorting them
+dataset = dataset[dataset['new_classification'] <= 2].groupby(by='province').count().sort_values(by='new_classification', ascending=False)
+dataset = dataset['new_classification']
+
+# Converting datas in list
+province = dataset.index.to_list()  # Take the province and insert in a list
+number = dataset.to_list()  # Take the numbers of the municipalities of every province and insert in a list
+
+print(province)
+print(number)
+print(dataset)
